2d35c71d9f4f2b7961a8793322df98d98d07d648
Carlos Henrique <ichfs9108@gmail.com>
2023-08-01 23:32:37 -0300

    Adicionando Entitie:Client e Dto:ClientDTO

---
+++ challenge/challenge/src/main/java/com/devsuperior/challenge/dto/ClientDTO.java
@@ -0,0 +1,78 @@
+package com.devsuperior.challenge.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+
+    private Long id;
+    @NotBlank(message = "Campo requerido")
+    private String name;
+    private String cpf;
+    private Double income;
+    @PastOrPresent
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO() {
+    }
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+}
