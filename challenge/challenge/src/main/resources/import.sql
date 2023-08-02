61f066b4612997cd21d36ad055a1d84ff944c9fa
Carlos Henrique <ichfs9108@gmail.com>
2023-08-02 00:09:26 -0300

    Adicionando seed de clientes

---
+++ challenge/challenge/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Aparecida', '24562987444', 256.00, '2012-06-12', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('João Aguilar', '20123254111', 150.00, '2011-04-01', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Thomas Aquino', '45874121066', 100.00, '2000-04-06', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Marco Pereira', '65987452322', 50.00, '1998-04-03', 10);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('João Nascimento', '45632178922', 56.00, '1990-02-19', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Pedro Santos', '74125896300', 25.00, '1995-03-14', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Felipe Flores', '95135745688', 21.00, '1985-07-26', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ronaldo Feitosa', '96585236274', 87.00, '1976-01-10', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Amarildo Fernandes', '10248965724', 6256.00, '2001-08-22', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Borges Souza', '03285425465', 289.00, '2004-12-26', 0);
